c1dfbddc03d6ba6e7d8fc14321f2320174139972
Ksiyeong <yuiop2767@naver.com>
2023-09-13 22:00:46 +0900

    [feat] Country 엔티티 추가

---
+++ src/main/java/com/echonrich/hr/domain/country/entity/Country.java
@@ -0,0 +1,22 @@
+package com.echonrich.hr.domain.country.entity;
+
+import com.echonrich.hr.domain.region.entity.Region;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "countries")
+@Getter
+@NoArgsConstructor
+public class Country {
+    @Id
+    @Column(length = 2, columnDefinition = "CHAR(2) NOT NULL")
+    private String countryId;
+
+    @Column(length = 40)
+    private String countryName;
+
+    @ManyToOne
+    @JoinColumn(name = "regionId", nullable = false)
+    private Region region;
+}
