1e7bc6266b75e0268638bb075f0c494e1e7f58b7
Ksiyeong <yuiop2767@naver.com>
2023-09-13 22:02:30 +0900

    [feat] JobHistory 엔티티 추가

---
+++ src/main/java/com/echonrich/hr/domain/job/entity/JobHistory.java
@@ -0,0 +1,34 @@
+package com.echonrich.hr.domain.job.entity;
+
+import com.echonrich.hr.domain.department.entity.Department;
+import com.echonrich.hr.domain.employee.entity.Employee;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class JobHistory {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "employeeId", nullable = false)
+    private Employee employee;
+
+    @Id
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Column(nullable = false)
+    private Date endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "jobId", nullable = false)
+    private Job job;
+
+    @ManyToOne
+    @JoinColumn(name = "departmentId", nullable = false)
+    private Department department;
+}
