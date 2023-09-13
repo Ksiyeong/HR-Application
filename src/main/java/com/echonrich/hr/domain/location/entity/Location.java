f70eee12fa1cb2297d5a32776e3bbdb0de18b6ad
Ksiyeong <yuiop2767@naver.com>
2023-09-13 22:01:03 +0900

    [feat] Location 엔티티 추가

---
+++ src/main/java/com/echonrich/hr/domain/location/entity/Location.java
@@ -0,0 +1,32 @@
+package com.echonrich.hr.domain.location.entity;
+
+import com.echonrich.hr.domain.country.entity.Country;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "locations")
+@Getter
+@NoArgsConstructor
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT(11) UNSIGNED")
+    private Long locationId;
+
+    @Column(length = 40)
+    private String streetAddress;
+
+    @Column(length = 12)
+    private String postalCode;
+
+    @Column(length = 30, nullable = false)
+    private String city;
+
+    @Column(length = 25)
+    private String stateProvince;
+
+    @ManyToOne
+    @JoinColumn(name = "countryId", nullable = false)
+    private Country country;
+}
