aa793264d5d16f6c5614de8aae5fe978950b3fad
Ksiyeong <yuiop2767@naver.com>
2023-09-13 22:01:27 +0900

    [feat] Department 엔티티 추가

---
+++ src/main/java/com/echonrich/hr/domain/department/entity/Department.java
@@ -0,0 +1,28 @@
+package com.echonrich.hr.domain.department.entity;
+
+import com.echonrich.hr.domain.employee.entity.Employee;
+import com.echonrich.hr.domain.location.entity.Location;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "departments")
+@Getter
+@NoArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT(11) UNSIGNED")
+    private Long departmentId;
+
+    @Column(length = 30, nullable = false)
+    private String departmentName;
+
+    @ManyToOne
+    @JoinColumn(name = "managerId")
+    private Employee manager;
+
+    @ManyToOne
+    @JoinColumn(name = "locationId", nullable = false)
+    private Location location;
+}
