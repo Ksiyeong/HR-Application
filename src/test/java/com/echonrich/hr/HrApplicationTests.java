e4c476ba552cccaa9284907412f578f28c504340
Ksiyeong <philip2767@gmail.com>
2023-09-14 12:02:16 +0900

    [test] HrApplicationTests 추가

---
+++ src/test/java/com/echonrich/hr/HrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echonrich.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
