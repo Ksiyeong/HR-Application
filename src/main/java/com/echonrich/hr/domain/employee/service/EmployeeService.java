803cc06f2d6585c2fc8f44cf1fea4357ff7d3317
Ksiyeong <philip2767@gmail.com>
2023-09-16 17:58:58 +0900

    [feat] EmployeeService 급여 변경 메서드 추가
    updateSalary
    updateSalaries

--- src/main/java/com/echonrich/hr/domain/employee/service/EmployeeService.java
+++ src/main/java/com/echonrich/hr/domain/employee/service/EmployeeService.java
@@ -1,7 +1,15 @@
 package com.echonrich.hr.domain.employee.service;
 
 import com.echonrich.hr.domain.employee.dto.EmployeeDto;
+import com.echonrich.hr.domain.employee.entity.Employee;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface EmployeeService {
     EmployeeDto.Response findEmployee(long employeeId);
+
+    void updateSalaries(List<Employee> employees, BigDecimal rate);
+
+    void updateSalary(long employeeId, BigDecimal rate);
 }
