19b2f82f63f8a9635f84c7e683ce9d53672bbf78
Ksiyeong <philip2767@gmail.com>
2023-09-16 18:02:37 +0900

    [feat] DepartmentService.findVerifiedDepartment 추가

--- src/main/java/com/echonrich/hr/domain/department/service/DepartmentService.java
+++ src/main/java/com/echonrich/hr/domain/department/service/DepartmentService.java
@@ -1,7 +1,10 @@
 package com.echonrich.hr.domain.department.service;
 
 import com.echonrich.hr.domain.department.dto.DepartmentDto;
+import com.echonrich.hr.domain.department.entity.Department;
 
 public interface DepartmentService {
     DepartmentDto.Response findDepartment(long departmentId);
+
+    Department findVerifiedDepartment(long departmentId);
 }
