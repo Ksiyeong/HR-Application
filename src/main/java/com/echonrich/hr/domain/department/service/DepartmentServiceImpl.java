4af7f662f38c7fd4d37c709b3715b52f378c6486
Ksiyeong <philip2767@gmail.com>
2023-09-16 18:00:36 +0900

    [refactor] EmployeeServiceImpl 트랜잭션 추가
    findVerifiedEmployee - readOnly = true

--- src/main/java/com/echonrich/hr/domain/department/service/DepartmentServiceImpl.java
+++ src/main/java/com/echonrich/hr/domain/department/service/DepartmentServiceImpl.java
@@ -25,10 +25,10 @@ public class DepartmentServiceImpl implements DepartmentService {
     }
 
     // 검증 로직
+    @Override
     @Transactional(readOnly = true)
     public Department findVerifiedDepartment(long departmentId) {
         return departmentRepository.findById(departmentId)
                 .orElseThrow(() -> new CustomLogicException(ExceptionCode.NOT_FOUND));
     }
-
 }
