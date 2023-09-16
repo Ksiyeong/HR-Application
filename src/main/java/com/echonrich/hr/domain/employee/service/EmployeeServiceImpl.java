a2af1a2d58ce8b07ac59dc72c862f3e7e72103e0
Ksiyeong <philip2767@gmail.com>
2023-09-16 17:59:40 +0900

    [feat] EmployeeServiceImpl 급여 변경 메서드 추가
    updateSalary
    updateSalaries

--- src/main/java/com/echonrich/hr/domain/employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/echonrich/hr/domain/employee/service/EmployeeServiceImpl.java
@@ -10,6 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -21,6 +25,36 @@ public class EmployeeServiceImpl implements EmployeeService {
     public EmployeeDto.Response findEmployee(long employeeId) {
         Employee employee = findVerifiedEmployee(employeeId);
         return employeeMapper.toEmployeeResponseDto(employee);
+    }
+
+    @Override
+    public void updateSalary(long employeeId, BigDecimal rate) {
+        Employee employee = findVerifiedEmployee(employeeId);
+        BigDecimal maxSalary = employee.getJob().getMaxSalary();
+        BigDecimal minSalary = employee.getJob().getMinSalary();
+
+        // 임금 계산
+        BigDecimal calculatedSalary = employee.getSalary().multiply(BigDecimal.ONE.add(rate));
+
+        // 최저 급여 보다 작을 경우 최저로 설정
+        if (calculatedSalary.compareTo(minSalary) < 0) {
+            employee.updateSalary(minSalary);
+
+            // 최고 급여 보다 클 경우 최고로 설정
+        } else if (calculatedSalary.compareTo(maxSalary) > 0) {
+            employee.updateSalary(maxSalary);
+
+            // 정상 범위일 경우 소수점 둘 째자리 까지 올림 처리
+        } else {
+            employee.updateSalary(calculatedSalary.setScale(2, RoundingMode.UP));
+        }
+    }
+
+    @Override
+    public void updateSalaries(List<Employee> employees, BigDecimal rate) {
+        for (Employee employee : employees) {
+            updateSalary(employee.getEmployeeId(), rate);
+        }
     }
 
     // 검증 로직
