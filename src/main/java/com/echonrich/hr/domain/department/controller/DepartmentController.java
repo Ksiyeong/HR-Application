61effa083c611ebe6c39b5d71f09ecf05ce254a4
Ksiyeong <philip2767@gmail.com>
2023-09-16 18:09:13 +0900

    [feat] 부서별 직원들의 급여 변경 api 추가
    DepartmentController.patchSalaryByDepartmentId 추가

--- src/main/java/com/echonrich/hr/domain/department/controller/DepartmentController.java
+++ src/main/java/com/echonrich/hr/domain/department/controller/DepartmentController.java
@@ -1,16 +1,16 @@
 package com.echonrich.hr.domain.department.controller;
 
 import com.echonrich.hr.domain.department.dto.DepartmentDto;
+import com.echonrich.hr.domain.department.entity.Department;
 import com.echonrich.hr.domain.department.service.DepartmentService;
+import com.echonrich.hr.domain.employee.dto.EmployeeDto;
+import com.echonrich.hr.domain.employee.service.EmployeeService;
 import com.echonrich.hr.global.response.SingleResponse;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,10 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/departments")
 public class DepartmentController {
     private final DepartmentService departmentService;
+    private final EmployeeService employeeService;
 
     @GetMapping("/{departmentId}")
-    public ResponseEntity getDepartment(@Positive @PathVariable long departmentId) {
+    public ResponseEntity getDepartment(@PathVariable @Positive long departmentId) {
         DepartmentDto.Response response = departmentService.findDepartment(departmentId);
         return ResponseEntity.ok(new SingleResponse<>(response));
     }
+
+    @PatchMapping("/{departmentId}/employees/salary")
+    public ResponseEntity patchSalaryByDepartmentId(@PathVariable @Positive long departmentId,
+                                                    @RequestBody EmployeeDto.SalaryRequest requestBody) {
+        Department department = departmentService.findVerifiedDepartment(departmentId);
+        employeeService.updateSalaryForEmployees(department.getEmployees(), requestBody.getRate());
+        return ResponseEntity.noContent().build();
+    }
+
 }
