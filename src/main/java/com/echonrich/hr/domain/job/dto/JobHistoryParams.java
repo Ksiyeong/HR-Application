4f98ea6b7e8e7d18a31fa47a4e6afc57ae15cc55
Ksiyeong <philip2767@gmail.com>
2023-09-17 17:39:21 +0900

    [refactor] JobHistoryParams
    @Range(min = 1L, max = 4294967295L) 추가
     - employee_id
     - department_id

--- src/main/java/com/echonrich/hr/domain/job/dto/JobHistoryParams.java
+++ src/main/java/com/echonrich/hr/domain/job/dto/JobHistoryParams.java
@@ -1,18 +1,19 @@
 package com.echonrich.hr.domain.job.dto;
 
-import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
 
 import java.sql.Date;
 
 @Getter
 @AllArgsConstructor
 public class JobHistoryParams {
-    @Min(1)
+    @Range(min = 1L, max = 4294967295L)
     private Long employee_id;
     private Date start_date;
     private Date end_date;
     private String job_id;
+    @Range(min = 1L, max = 4294967295L)
     private Long department_id;
 }
