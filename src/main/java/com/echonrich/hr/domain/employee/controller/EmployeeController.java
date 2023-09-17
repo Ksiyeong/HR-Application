eeff1123cec34724d78f6964d2f3c46ff5d6caec
Ksiyeong <philip2767@gmail.com>
2023-09-17 21:45:15 +0900

    [refactor] EmployeeController
    employeeId 유효성 검사 변경
    @Range(min = 1L, max = 4294967295L)

--- src/main/java/com/echonrich/hr/domain/employee/controller/EmployeeController.java
+++ src/main/java/com/echonrich/hr/domain/employee/controller/EmployeeController.java
@@ -3,8 +3,8 @@ package com.echonrich.hr.domain.employee.controller;
 import com.echonrich.hr.domain.employee.dto.EmployeeDto;
 import com.echonrich.hr.domain.employee.service.EmployeeService;
 import com.echonrich.hr.global.response.SingleResponse;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +20,7 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @GetMapping("/{employeeId}")
-    public ResponseEntity getEmployee(@Positive @PathVariable long employeeId) {
+    public ResponseEntity getEmployee(@PathVariable @Range(min = 1L, max = 4294967295L) long employeeId) {
         EmployeeDto.Response response = employeeService.findEmployee(employeeId);
         return ResponseEntity.ok(new SingleResponse<>(response));
     }
