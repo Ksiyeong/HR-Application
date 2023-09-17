1c956e45d3999bd53131051d0f29f65fe74985ae
Ksiyeong <philip2767@gmail.com>
2023-09-17 12:04:02 +0900

    [feat] ArirangClientImpl
    아리랑국제뉴스api 요청 클라이언트 구현클래스

---
+++ src/main/java/com/echonrich/hr/infrastructure/arirang/client/ArirangClientImpl.java
@@ -0,0 +1,44 @@
+package com.echonrich.hr.infrastructure.arirang.client;
+
+import com.echonrich.hr.infrastructure.arirang.dto.ArirangItemsResponseDto;
+import com.echonrich.hr.infrastructure.arirang.dto.ArirangResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Component
+@RequiredArgsConstructor
+public class ArirangClientImpl implements ArirangClient {
+    private final WebClient webClient;
+    @Value("${infrastructure.arirang.service-key}")
+    private String enCodedArirangServiceKey;
+
+    @Override
+    public Page<ArirangItemsResponseDto> findArirangNews(int page, int size) {
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://apis.data.go.kr/B551024/openArirangNewsApi/news")
+                .queryParam("serviceKey", enCodedArirangServiceKey)
+                .queryParam("pageNo", page)
+                .queryParam("numOfRows", size)
+                .build(true)
+                .toUri();
+
+        ArirangResponseDto arirangResponseDto = webClient
+                .get()
+                .uri(uri)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(ArirangResponseDto.class)
+                .block();
+
+        return new PageImpl<>(arirangResponseDto.getItems(), PageRequest.of(page - 1, size), arirangResponseDto.getTotalCount());
+    }
+}
